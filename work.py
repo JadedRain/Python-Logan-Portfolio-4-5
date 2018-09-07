fcacbe0325bfe0d764afd5b4497951c90d71015e
Logan Douglas <logan.douglas@tooeleschools.org>
2018-09-07 13:25:48 -0600

    First commit, 4/5th Madlib and str to int chapter 1

---
+++ work.py
@@ -0,0 +1,31 @@
+score1 = input("Score")
+score2 = input("Score")
+score3 = input("Score")
+score4 = input("score")               
+score5 = input("score")
+sum1 = int(score1) + int(score2) + int(score3) + int(score4) + int(score5)
+avg = sum1/5
+print(avg)
+
+
+
+
+
+##adjec1 = input("Adjective ")
+##noun1 = input("Noun ")
+##plural_noun1 = input("Plural noun ")
+##person1 = input("Person ")
+##adjec2 = input("Adjective ")
+##clothing1 = input("Clothing ")
+##noun2 = input("Noun ")
+##city1 = input("City ")
+##plural_noun2 = input("Plural noun ")
+##
+##print('The ' + adjec1 + ' ' + noun1 + ' Decided to buy some ' + plural_noun1 +
+##'. After that they met up with '  + person1 + ' Heading out to the ' + adjec2 + ' lake wearing a '
+##      + clothing1 + ' holding an ' + noun2 + '. When they finished they went to ' + city1 +
+##      ' Grabbing some ' + plural_noun2 + ' before going back home.')
+##
+input()
+
+
