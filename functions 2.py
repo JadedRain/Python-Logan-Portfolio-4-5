94a838b37ce8998f66d556f89856c47ddefceee9
Logan Douglas <logan.douglas@tooeleschools.org>
2018-09-13 13:54:15 -0600

    Function example in class

---
+++ functions 2.py
@@ -0,0 +1,13 @@
+#Logan Douglas
+#9/13
+
+#Get a users name
+def get_name():
+#Step 1:Promp the user for their name.
+  name = input("What is your name?")
+#Step 2: Display the name back to the user.
+  print("Your name is currently", name)
+#Step 3: Verify that the name was correct.
+  input("Is this your name? Yes or No")
+
+  
