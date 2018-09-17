469cf1917a79d8c0d01bcfa208f0cc20fee59a6d
Logan Douglas <logan.douglas@tooeleschools.org>
2018-09-17 14:02:49 -0600

    Functions with params examples

--- functions 2.py
+++ functions 2.py
@@ -1,13 +1,48 @@
 #Logan Douglas
 #9/13
 
+
+import math
+
 #Get a users name
-def get_name():
-#Step 1:Promp the user for their name.
-  name = input("What is your name?")
+def get_name(name):
+  input_name = name
+  input_name = input_name.lower()
+  input_name = input_name.title()
+
 #Step 2: Display the name back to the user.
-  print("Your name is currently", name)
+  print("Your name is currently", input_name)
 #Step 3: Verify that the name was correct.
   input("Is this your name? Yes or No")
+name = input("What is your name?")
+get_name(name)
 
+
+def pythagoras_theorem(ap = 1,bp = 1):
+  a = float(ap)
+  b = float(bp)
+  c = a*a+b*b
+  c = math.sqrt(c)
+
+  print("The third side is", c)
+
+
+
+##pythagoras_theorem(ai,bi)
+
+
+
+
+def add_numbers(x,y):
+  num1 = x
+  print("num1 = x ",num1)
+  num2 = y
+  print("num2 = y",num2)
+  num3 = int(num1) + int(num2)
+  return num3
+
+x = 1
+y = 10
+##num4 = add_numbers(x,y)
+print(num4)
   
